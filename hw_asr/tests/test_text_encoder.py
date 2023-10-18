480d0296aab01f068dcfb72244f989cb4be953d8
TrickmanOff <egorkadv2002@gmail.com>
2023-10-18 18:15:57 +0300

    finish ctc-encoder (beam search + tests)

--- hw_asr/tests/test_text_encoder.py
+++ hw_asr/tests/test_text_encoder.py
@@ -1,6 +1,9 @@
 import unittest
+from typing import List, Tuple
 
-from hw_asr.text_encoder.ctc_char_text_encoder import CTCCharTextEncoder
+import torch
+
+from hw_asr.text_encoder.ctc_char_text_encoder import CTCCharTextEncoder, Hypothesis
 
 
 class TestTextEncoder(unittest.TestCase):
@@ -13,6 +16,40 @@ class TestTextEncoder(unittest.TestCase):
         decoded_text = text_encoder.ctc_decode(inds)
         self.assertIn(decoded_text, true_text)
 
+    def _check_beam_search_res(self, decoded: List[Hypothesis], expected: List[Tuple]):
+        self.assertEqual(len(decoded), len(expected))
+        for i, (text, prob) in enumerate(expected):
+            self.assertEqual(decoded[i].text, text)
+            self.assertAlmostEqual(decoded[i].prob, prob)
+
     def test_beam_search(self):
-        # TODO: (optional) write tests for beam search
-        pass
+        text_encoder = CTCCharTextEncoder()
+
+        # initialize probabilities
+        empty_token_ind = text_encoder.char2ind[text_encoder.EMPTY_TOK]
+        a_ind = text_encoder.char2ind['a']
+        b_ind = text_encoder.char2ind['b']
+
+        probs = torch.zeros(2, len(text_encoder))
+
+        probs[0][empty_token_ind] = 0.3
+        probs[0][a_ind] = 0.2
+        probs[0][b_ind] = 0.5
+
+        probs[1][empty_token_ind] = 0.6
+        probs[1][a_ind] = 0.4
+
+        # beam_size = 1
+        decoded = text_encoder.ctc_beam_search(probs, 2, beam_size=1)
+        expected = [('b', 0.3)]
+        self._check_beam_search_res(decoded, expected)
+
+        # beam_size = 2
+        decoded = text_encoder.ctc_beam_search(probs, 2, beam_size=2)
+        expected = [('b', 0.3), ('ba', 0.2)]
+        self._check_beam_search_res(decoded, expected)
+
+        # beam_size = 3
+        decoded = text_encoder.ctc_beam_search(probs, 2, beam_size=3)
+        expected = [('b', 0.3), ('ba', 0.2), ('', 0.18)]
+        self._check_beam_search_res(decoded, expected)
--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -1,8 +1,14 @@
-from typing import List, NamedTuple
+from collections import defaultdict
+from typing import List, NamedTuple, DefaultDict
 
 import torch
 
 from .char_text_encoder import CharTextEncoder
+
+
+class IndexHypothesis(NamedTuple):
+    inds: List[int]
+    prob: float
 
 
 class Hypothesis(NamedTuple):
@@ -18,10 +24,21 @@ class CTCCharTextEncoder(CharTextEncoder):
         vocab = [self.EMPTY_TOK] + list(self.alphabet)
         self.ind2char = dict(enumerate(vocab))
         self.char2ind = {v: k for k, v in self.ind2char.items()}
+        self.alphabet = vocab
 
     def ctc_decode(self, inds: List[int]) -> str:
-        # TODO: your code here
-        raise NotImplementedError()
+        text = []
+        prev_ind = None
+
+        for i in range(len(inds) + 1):
+            ind = inds[i] if i < len(inds) else None
+            if ind != prev_ind:
+                if prev_ind is not None:
+                    prev_ch = self.ind2char[prev_ind]
+                    if prev_ch != self.EMPTY_TOK:
+                        text.append(prev_ch)
+                prev_ind = ind
+        return ''.join(text)
 
     def ctc_beam_search(self, probs: torch.tensor, probs_length,
                         beam_size: int = 100) -> List[Hypothesis]:
@@ -31,7 +48,27 @@ class CTCCharTextEncoder(CharTextEncoder):
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
-        hypos: List[Hypothesis] = []
-        # TODO: your code here
-        raise NotImplementedError
-        return sorted(hypos, key=lambda x: x.prob, reverse=True)
+
+        hypos: List[IndexHypothesis] = [IndexHypothesis([], 1.)]
+
+        for ind_probs in probs[:probs_length]:
+            hypos_probs = torch.tensor([hyp.prob for hyp in hypos])
+            prod_probs = hypos_probs.outer(ind_probs)
+            top_indices = prod_probs.flatten().topk(beam_size).indices
+            top_indices = [(i.item() // len(ind_probs), i.item() % len(ind_probs))
+                           for i in top_indices]
+            hypos = [
+                IndexHypothesis(hypos[hypo_index].inds + [new_ind],
+                                prod_probs[hypo_index, new_ind])
+                for hypo_index, new_ind in top_indices
+            ]
+
+        text_hypos_map: DefaultDict[str, float] = defaultdict(float)
+        for hypo in hypos:
+            text_hypos_map[self.ctc_decode(hypo.inds)] += hypo.prob
+
+        text_hypos: List[Hypothesis] = [
+            Hypothesis(text, prob) for text, prob in text_hypos_map.items()
+        ]
+
+        return sorted(text_hypos, key=lambda x: x.prob, reverse=True)
