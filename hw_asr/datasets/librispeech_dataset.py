5a74c47271a71fdc8700760973f020184dd5444e
TrickmanOff <egorkadv2002@gmail.com>
2023-10-21 00:39:46 +0300

    fix my config for kaggle

--- hw_asr/datasets/librispeech_dataset.py
+++ hw_asr/datasets/librispeech_dataset.py
@@ -3,6 +3,7 @@ import logging
 import os
 import shutil
 from pathlib import Path
+from typing import Optional
 
 import torchaudio
 from speechbrain.utils.data_utils import download_file
@@ -26,16 +27,19 @@ URL_LINKS = {
 
 
 class LibrispeechDataset(BaseDataset):
-    def __init__(self, part, config_parser: ConfigParser, data_dir=None, *args, **kwargs):
+    def __init__(self, part, config_parser: ConfigParser, data_dir=None, index_dir=None, *args, **kwargs):
         assert part in URL_LINKS or part == 'train_all'
 
         if isinstance(data_dir, str):
             data_dir = Path(data_dir)
+        if isinstance(index_dir, str):
+            index_dir = Path(index_dir)
 
         if data_dir is None:
             data_dir = config_parser.get_data_root_dir() / "data" / "datasets" / "librispeech"
             data_dir.mkdir(exist_ok=True, parents=True)
         self._data_dir = data_dir
+        self._index_dir = data_dir if index_dir is None else index_dir
         if part == 'train_all':
             index = sum([self._get_or_load_index(part)
                          for part in URL_LINKS if 'train' in part], [])
@@ -55,7 +59,8 @@ class LibrispeechDataset(BaseDataset):
         shutil.rmtree(str(self._data_dir / "LibriSpeech"))
 
     def _get_or_load_index(self, part):
-        index_path = self._data_dir / f"{part}_index.json"
+        index_path = self._index_dir / f"{part}_index.json"
+
         if index_path.exists():
             with index_path.open() as f:
                 index = json.load(f)
