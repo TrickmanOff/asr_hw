9f8414c28794287ac45d424b7102e7584ce13e8c
TrickmanOff <egorkadv2002@gmail.com>
2023-10-26 21:37:42 +0300

    add RandomNoise augmentation

---
+++ hw_asr/augmentations/wave_augmentations/RandomNoise.py
@@ -0,0 +1,25 @@
+import numpy as np
+from torch import Tensor
+
+from hw_asr.augmentations.base import AugmentationBase
+from hw_asr.augmentations.random_apply import RandomApply
+
+
+class RandomNoiseBase(AugmentationBase):
+    def __init__(self, max_noise_ampl: float = 5e-3):
+        self.max_noise_ampl = max_noise_ampl
+
+    def __call__(self, audio: Tensor) -> Tensor:
+        noise_ampl = np.random.uniform(0, self.max_noise_ampl)
+        noise = np.random.normal(size=audio.shape)
+        noise /= np.abs(noise).max()  # [-1, 1]
+        noised_audio = audio + noise_ampl / np.abs(audio).max() * noise
+        return noised_audio
+
+
+class RandomNoise(AugmentationBase):
+    def __init__(self, p: float = 1., *args, **kwargs):
+        self._aug = RandomApply(RandomNoiseBase(*args, **kwargs), p=p)
+
+    def __call__(self, audio: Tensor) -> Tensor:
+        return self._aug(audio)
