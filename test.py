c8bc1310e4f0e2ab9d573508914a1d405fd865dc
TrickmanOff <egorkadv2002@gmail.com>
2023-10-27 23:56:46 +0300

    refactor

--- test.py
+++ test.py
@@ -84,12 +84,9 @@ def main(config, out_file):
                     {
                         "ground_truth": batch["text"][i],
                         "pred_text_argmax": text_encoder.ctc_decode(argmax.cpu().numpy()),
-                        # "pred_text_beam_search": text_encoder.ctc_beam_search(
-                        #     batch["probs"][i], batch["log_probs_length"][i], beam_size=10
-                        # )[:10],
-                        # "pred_text_beam_search": text_encoder.ctc_beam_search(
-                        #     batch["probs"][i], batch["log_probs_length"][i], beam_size=10
-                        # )[:100],
+                        "pred_text_beam_search": text_encoder.ctc_beam_search(
+                            batch["probs"][i], batch["log_probs_length"][i], beam_size=100
+                        )[:10],
                     }
                 )
                 if lm_beam_search_text_preds is not None:
